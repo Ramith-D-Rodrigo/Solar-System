b33f24574606bc767d77d92df7fc24c39aee53bd
Ramith Rodrigo <ramithrodrigo@yahoo.com>
2023-07-29 14:00:51 +0530

    saturn fixed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from './node_modules/three/src/Three.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import planets from './planets.js';
 
-const SEGMENTS = 500;
+const SEGMENTS = 100;
 
 const CAMERA_START_X = -0.1;
 const CAMERA_START_Y = 30;
@@ -16,6 +16,9 @@ let controls = null;
 let SunMesh = null;
 const sunRotationPoints = [];
 let renderer = null; //renderer
+let saturnMoonRotationPoints = [];
+let moons = [];
+let textureLoader = null;
 
 const init = () => {
     scene = new THREE.Scene(); //create a scene
@@ -30,23 +33,16 @@ const init = () => {
         '8k_stars_milky_way_cube.jpg',
     ]);
 
-    renderer = new THREE.WebGLRenderer(); //create a renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true }); //create a renderer
     renderer.setSize(window.innerWidth, window.innerHeight); //set the size of the renderer
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     document.body.appendChild(renderer.domElement);
      
     controls = new OrbitControls(camera, renderer.domElement); //create controls for the camera
-    controls.listenToKeyEvents(window); //listen to key events
-    
-    //controls keys
-    controls.keys = {
-        LEFT: 'KeyA',
-        UP: 'KeyW',
-        RIGHT: 'KeyD',
-        BOTTOM: 'KeyS'
-    }
-    
+        
     //texture loader
-    const textureLoader = new THREE.TextureLoader();
+    textureLoader = new THREE.TextureLoader();
         
     const sunGeometry = new THREE.SphereGeometry(30, SEGMENTS, SEGMENTS); //create a sphere
     const sunTexture = textureLoader.load('./textures/2k_sun.jpg');
@@ -55,8 +51,8 @@ const init = () => {
     SunMesh = new THREE.Mesh(sunGeometry, SunMaterial); //create a mesh
     scene.add(SunMesh); //add the mesh to the scene
     
-    const sunLight = new THREE.PointLight(0xFFFFFF, 2, 500); //create a light
-    sunLight.position.set(0, 0, 0); //set the light's position
+    const sunLight = new THREE.PointLight(0xFFFFFF, 1.5, 700); //create a light
+    sunLight.position.set(0, 30, 0); //set the light's position
     sunLight.castShadow = true;
     scene.add(sunLight); //add the light to the scene
         
@@ -72,8 +68,10 @@ const init = () => {
         const planetGeometry = new THREE.SphereGeometry(planet.radius, SEGMENTS, SEGMENTS); //create a sphere
         const planetTexture = textureLoader.load(planet.texture);
         const planetMaterial = new THREE.MeshStandardMaterial({ map: planetTexture }); //create a material
-        planetMaterial.castShadow = true;
         const planetMesh = new THREE.Mesh(planetGeometry, planetMaterial); //create a mesh
+
+        planetMesh.castShadow = true;
+        planetMesh.receiveShadow = true;
     
         //add planet relative to the sun's coordinate system
         let position = null;
@@ -92,31 +90,66 @@ const init = () => {
             //create the moon
             const moonGeometry = new THREE.SphereGeometry(1, SEGMENTS, SEGMENTS); //create a sphere
             const moonTexture = textureLoader.load('./textures/2k_moon.jpg');
-            const moonMaterial = new THREE.MeshPhysicalMaterial({ map: moonTexture }); //create a material
+            const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture }); //create a material
             const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial); //create a mesh
+
+            moonMesh.castShadow = true;
+            moonMesh.receiveShadow = true;
+
             moonMesh.position.x = 10;
             planetMesh.add(moonMesh); //add the mesh to the scene   
+
+            moons.push(moonMesh);
         }
     
         if(planet.name === "Saturn"){
             //create the ring
+
+            const ringColors = ['#655f45', '#d8ae6d', '#ffe1ab', '#dbb57c', '#b89c72', '#a98b6e', '#8a7a6a'];
             for(let j = 0; j < 7; j++){
-                const ringGeometry = new THREE.RingGeometry((planets[i].radius + 0.1) + j * 0.5, (planets[i].radius + 0.5) + j * 0.5, 32);
-                const ringMaterial = new THREE.MeshToonMaterial({ side: THREE.DoubleSide });
-                const ringTexture = textureLoader.load('./textures/2k_saturn_ring_alpha.png');
-                ringMaterial.transparent = true;
-                ringMaterial.castShadow = true;
-                ringMaterial.map = ringTexture;
+                const ringGeometry = new THREE.RingGeometry((planets[i].radius + 0.5) + j * 0.3, (planets[i].radius + 0.8) + j * 0.3, SEGMENTS, SEGMENTS);
+                const ringMaterial = new THREE.MeshStandardMaterial({ color: ringColors[j], side: THREE.DoubleSide });
+
                 const ringMesh = new THREE.Mesh(ringGeometry, ringMaterial); //create a mesh
                 ringMesh.rotation.x = Math.PI / 2;
                 planetMesh.add(ringMesh); //add the mesh to the scene
+
+                ringMesh.castShadow = false;
+                ringMesh.receiveShadow = true;
+            } 
+            
+            //create the moons of saturn (create 10 moons)
+            for(let j = 0; j < 10; j++){
+                //create rotation point for the moon
+                const moonRotationPoint = new THREE.Object3D();
+                //give random x rotation for random axis for the moon
+                moonRotationPoint.rotation.x = Math.random() * 2 * Math.PI;
+
+                //add the moon rotation point to the planet
+                planetMesh.add(moonRotationPoint);
+
+
+                const moonGeometry = new THREE.SphereGeometry(Math.random() * 0.45, SEGMENTS, SEGMENTS); //create a sphere
+                const moonTexture = textureLoader.load('./textures/2k_moon.jpg');
+                const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture }); //create a material
+                const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial); //create a mesh
+
+                moonMesh.castShadow = true;
+                moonMesh.receiveShadow = true;
+
+                moonMesh.position.x = planets[i].radius + 10 + j * 1.5;
+                //add the moon to the moon rotation point
+                moonRotationPoint.add(moonMesh); //add the mesh to the scene   
+
+                saturnMoonRotationPoints.push([moonRotationPoint, Math.random() * 0.007]); //add the moon rotation point to the array and give it a random rotation speed
+
+                moons.push(moonMesh);
             }
-    
         }
     
         planet.setMesh(planetMesh);
     }
-    
+
     //set the camera position
     camera.position.z = CAMERA_START_Z;
     camera.position.y = CAMERA_START_Y; 
@@ -149,6 +182,18 @@ const rotatePlanets = () => {
     }
 }
 
+const saturnMoonRotaion = () => {
+    for(let i = 0; i < saturnMoonRotationPoints.length; i++){
+        saturnMoonRotationPoints[i][0].rotation.y += saturnMoonRotationPoints[i][1];
+    }
+}
+
+const rotateMoons = () => {
+    for(let i = 0; i < moons.length; i++){
+        moons[i].rotation.y += 0.005;
+    }
+}
+
 //object focus
 
 const followObject = () => {
@@ -158,14 +203,16 @@ const followObject = () => {
         followingObj.getWorldPosition(objectPosition);
         const cameraOffset = new THREE.Vector3(0, 5, followingObj.geometry.boundingSphere.radius + 10);
         camera.position.copy(objectPosition).add(cameraOffset);
+        camera.lookAt(objectPosition);
         controls.enabled = false;
     }
 }
-
 const animate = () => {
     requestAnimationFrame(animate);
     rotateSun();
     rotatePlanets();
+    rotateMoons();
+    saturnMoonRotaion();
     followObject();
 
     renderer.render(scene, camera); //render the scene
@@ -206,7 +253,6 @@ const focusObject = (event) => {
     if(intersects.length > 0){
         followingObj = intersects[0].object; //get the first intersected object
         isFollowingObj = true;
-        console.log(followingObj);
     }
 }
 
@@ -214,7 +260,6 @@ const focusObject = (event) => {
 document.addEventListener('mousemove', mouseMove);
 document.addEventListener('click', focusObject);
 document.addEventListener("keydown", stopFollowingObject);
-
 //rendering
 init();
 animate();
